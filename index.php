6dbad1f4f25de4907eeb1d4478f8edf929ef58b6
Brian Ramsay <brian@foont.net>
2011-03-24 11:38:45 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Swords &amp; Wizardry Tools</title>
+	<link HREF="sw.css" media='screen' rel="stylesheet" TYPE="text/css">
+	<meta http-equiv="X-UA-Compatible" content="chrome=1">
+	<meta name="description" content="Quick calculations for Swords & Wizardry.">
+<!-- Google Analytics init -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+	_gaq.push(
+  	['_setAccount', 'UA-9377034-1'],
+  	['_setDomain', '.foont.net'],
+  	['_trackPageview']
+	);
+</script>
+</head>
+<body>
+		<ul class="nav">
+				<li>Treasure</li>
+		</ul>
+		<h2>Create Treasure</h2>
+		<script src="tinytab.js"></script>
+</body>
+</html>
