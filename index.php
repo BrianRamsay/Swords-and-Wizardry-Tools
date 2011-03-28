e19141d99729a54ffec205b6190a8965329c4b1d
Brian Ramsay <brian@foont.net>
2011-03-28 15:04:30 -0700

    forgot to actually fire off the google analytics code

--- index.php
+++ index.php
@@ -50,6 +50,11 @@
 			tb = new TinyTab($$('ul.nav li'),$$('ul.content li'));
 			Tools.init();
 		});
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
 	</script>
 	<script src="js/ienotify.js"></script>
 </body>
